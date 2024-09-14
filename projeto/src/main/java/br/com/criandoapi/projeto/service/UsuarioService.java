55c2a79f94f784329929eaf36abbe027c4b0e84b
kaio <kaio.devfull@gmail.com>
2024-09-14 00:06:13 -0300

    Api de teste

---
+++ projeto/src/main/java/br/com/criandoapi/projeto/service/UsuarioService.java
@@ -0,0 +1,72 @@
+package br.com.criandoapi.projeto.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.criandoapi.projeto.dto.UsuarioDto;
+import br.com.criandoapi.projeto.model.Usuario;
+import br.com.criandoapi.projeto.repository.IUsuario;
+import br.com.criandoapi.projeto.security.Token;
+import br.com.criandoapi.projeto.security.TokenUtil;
+import jakarta.validation.Valid;
+
+@Service
+public class UsuarioService {
+
+	private IUsuario repository;
+	private PasswordEncoder passwordEncoder;
+
+	public UsuarioService(IUsuario repository) {
+		this.repository = repository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
+	}
+
+	public List<Usuario> listarUsuario() {
+		List<Usuario> lista = repository.findAll();
+		return lista;
+	}
+
+	public Usuario criarUsuario(Usuario usuario) {
+		String encoder = this.passwordEncoder.encode(usuario.getSenha());
+		usuario.setSenha(encoder);
+		Usuario usuarioNovo = repository.save(usuario);
+		return usuarioNovo;
+	}
+
+	public Usuario editarUsuario(Usuario usuario) {
+		String encoder = this.passwordEncoder.encode(usuario.getSenha());
+		usuario.setSenha(encoder);
+		Usuario usuarioNovo = repository.save(usuario);
+		return usuarioNovo;
+	}
+
+	public Boolean excluirUsuario(Integer id) {
+		repository.deleteById(id);
+		return true;
+	}
+
+	public Boolean validarSenha(Usuario usuario) {
+		Optional<Usuario> optionalUsuario = repository.findById(usuario.getId());
+		if (optionalUsuario.isPresent()) {
+			String senha = optionalUsuario.get().getSenha();
+			Boolean valid = passwordEncoder.matches(usuario.getSenha(), senha);
+			return valid;
+		}
+		return false;
+	}
+
+	public Token gerarToken(@Valid UsuarioDto usuario) {
+		Usuario user = repository.findBynomeOrEmail(usuario.getNome(), usuario.getEmail());
+		if (user != null) {
+			Boolean valid = passwordEncoder.matches(usuario.getSenha(), user.getSenha());
+			if (valid) {
+				return new Token(TokenUtil.createToken(user));
+			}
+		}
+		return null;
+	}
+}
